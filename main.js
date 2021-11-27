319294c2383dcda8cd3f4fd600ac40740e5314d9
eimer <eimerlegui98@gmail.com>
2021-11-27 18:19:21 -0500

    Guardado en LS

---
+++ main.js
@@ -0,0 +1,91 @@
+const recetasFav = document.getElementById("ul-fav")
+const recetas = document.getElementById("recetas");
+const busqueda = document.getElementById("busqueda");
+
+getRandomReceta();
+
+busqueda.addEventListener('click', async ()=>{
+    const input = document.getElementById("input");
+    recetas.innerHTML = " ";
+    await getBusquedaReceta(input.value);
+})
+
+//Optiene e imprime receta random
+async function getRandomReceta(){
+    for (let i = 1; i < 7; i++) {
+        let resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+        let respData = await resp.json();
+        let randomReceta = respData.meals[0];   
+         
+        addRecetas(randomReceta);
+        addFav(randomReceta);
+    }
+};
+
+//Optiene e imprime receta buscada
+async function getBusquedaReceta(consulta){
+    let resp = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+consulta);
+    let respData = await resp.json();
+    let receta = respData.meals;
+
+    if (receta) {
+        receta.forEach((receta) => {
+            addRecetas(receta);                    
+        });        
+    }else{
+        alert("No se encuentran resultados de "+consulta)
+    }
+}
+
+//Inserta en el Dom las tarjetas de las recetas
+function addRecetas (recetain){
+    let receta = document.createElement('div');
+    receta.classList.add('card-recetas');    
+    receta.innerHTML = `
+                <div class="img-receta">
+                    <img src="${recetain.strMealThumb}" alt="${recetain.strMeal}">                
+                </div>
+                <div class="pie-receta">
+                    <h4>${recetain.strMeal}</h4>
+                    <button class="fav-btn"><i class="fas fa-heart"></i></button>
+                </div>
+    `;
+    
+    const fav_btn = receta.querySelector(".pie-receta .fav-btn");
+    fav_btn.addEventListener("click", ()=>{
+        if (!fav_btn.classList.contains('active')) {            
+            fav_btn.classList.add('active');            
+            addLS(recetain.idMeal);
+        }else{                        
+            fav_btn.classList.remove('active');
+        }        
+    })
+    recetas.appendChild(receta);
+};
+
+//Insertar en el Dom los Favoritos
+function addFav (recetain){    
+    let recetaFav = document.createElement('li');
+    recetaFav.classList.add('li-fav');
+    recetaFav.innerHTML = `
+    <img src="${recetain.strMealThumb}" alt="">
+    <br><span>${recetain.strMeal}</span>
+    <botton class="clos"><i class="far fa-times-circle"></i></botton>
+    `;
+    recetasFav.appendChild(recetaFav);
+}
+const todos = [];
+function addLS (recetaid){
+
+    const todos = JSON.parse(localStorage.getItem('idrecetas')) || [];
+    todos.push(recetaid);
+    const todosJ = JSON.stringify(todos)
+    localStorage.setItem('idrecetas', todosJ)
+    console.log(todos);
+
+}
+
+function getLS (){
+    const idrecetas = JSON.parse(localStorage.getItem('idrecetas')) || [];
+    return idrecetas;
+}
